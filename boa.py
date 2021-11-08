857a688e090c412e739da9fbf94df268d03cf6d6
nosayana <nenagbare@gmail.com>
2021-11-07 17:19:07 -0800

    additional training

---
+++ boa.py
@@ -0,0 +1,19 @@
+
+#git branch : It gives the branch names in current repository.
+#git branch bugfix : It will create the bugfix branch in local git repository.
+#git branch -v: It will display all the branches in your repo, and also tell you what branch you're currently in.
+bugfix 87226db initial commit
+* master 87226db initial commit
+Note: Here * indicate currently in use branch.
+# git checkout bugfix : Switch to bugfix branch. Switched to
+branch 'bugfix'
+Update the land.txt like change 2 – bugfix branch
+# git add . : Add one or more files to staging
+# git commit -m "bugfix commit"
+# git checkout master : Switch to master branch. Switched to
+branch 'master'
+Updat the Bhaskar.txt like change 3 – master branch
+# git add .
+# git commit -m "master commit"
+# git checkout bugfix : Switch to bugfix branch. Switched to
+branch 'bugfi
